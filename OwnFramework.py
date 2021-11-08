082adf708cf17f011505578e767f2b64d43f55eb
Hiko <amir.amuri@yahoo.de>
2021-11-08 13:02:12 +0100

    Create OwnFramework.py
    Started coding on an reader for text-input

---
+++ OwnFramework.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  8 12:08:04 2021
+
+@author: mHiko
+"""
+
+class NluBasic():
+    def requestMessage():
+        msg = input("Please Input an answer: ")
+        return msg
+    def readMessage():
+        msg = NluBasic.requestMessage()
+        
+        return print(f"You wrote following message: {msg} ")
+    
+    readMessage()
+
+    
