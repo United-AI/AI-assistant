c5b1604ee7973d7c149e96218361912d15720e1a
Hiko <amir.amuri@yahoo.de>
2021-11-09 17:18:38 +0100

    Updating the Framework
    Bot is reading possible greetings out of helloDict.txt and possible answers to his first questions out of howAreYouDict.txt
    
    Currently only limited access, the lists are editable and the bot will always use the newest one once started.
    
    If he doesn't find the answer in the set he will answer for now with "error103"
    
    Code will be commented in the next commit.
    
    So far, bot can read and write.
    
    Need to improve this and add a method to learn from user.

--- OwnFramework.py
+++ OwnFramework.py
@@ -6,13 +6,34 @@ Created on Mon Nov  8 12:08:04 2021
 """
 
 class NluBasic():
+    helloDict = set()
+    howAreYouDict = set()
+    with open('helloDict.txt') as f:
+        for line in f:
+            key= line.strip()
+            helloDict.add(key)
+    with open('howAreYouDict.txt') as f:
+        for line in f:
+            key= line.strip()
+            howAreYouDict.add(key)
+    msg = ""
     def requestMessage():
-        msg = input("Please Input an answer: ")
-        return msg
+        NluBasic.msg = input("Please Input an answer: ")
+        pass
+    
+    def _answer():
+        if NluBasic.msg in NluBasic.helloDict:
+            botMSG = "Hey, how are you?"
+        elif NluBasic.msg in NluBasic.howAreYouDict:
+            botMSG = "Thank you for using me, what can I help you with? "
+        else:
+           botMSG ="error103"
+        return print(f"UnitedAi Bot: {botMSG}")
     
     def __init__(self):
-        msg = NluBasic.requestMessage()
-        return print(f"You wrote following message: {msg} ")
+        while True:
+            NluBasic.requestMessage()
+            NluBasic._answer()
 
 NluBasic()
     
